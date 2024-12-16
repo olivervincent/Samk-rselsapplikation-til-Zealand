791d6ca48011f35d6610604da0dd212f4e01be1e
Glunzhammeken <160604689+Glunzhammeken@users.noreply.github.com>
2024-12-16 10:09:30 +0100

    hjej

--- EventsPlanner/Startup.cs
+++ EventsPlanner/Startup.cs
@@ -44,7 +44,7 @@ namespace EventsPlanner
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseDeveloperExceptionPage();
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
