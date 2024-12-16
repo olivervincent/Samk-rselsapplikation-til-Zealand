4c4b5fdb3e507699c3b42b39dd86aece874eda31
Glunzhammeken <160604689+Glunzhammeken@users.noreply.github.com>
2024-12-16 21:22:26 +0100

    asda

--- EventsPlanner/Pages/Bookings/UpdateBookings.cshtml.cs
+++ EventsPlanner/Pages/Bookings/UpdateBookings.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using EventsPlanner.Models;
 using EventsPlanner.Services.Interface;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace EventsPlanner.Pages.Bookings
 {
@@ -10,17 +11,30 @@ namespace EventsPlanner.Pages.Bookings
     {
         [BindProperty]
         public Booking Booking { get; set; }
+        [BindProperty]
+        public List<Stand> Stands { get; set; }
+        [BindProperty]
+        public List<Guest> Guests { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int StandNo { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int GuestNo { get; set; }
         private readonly IBookingService _service;
+        private readonly IStandService _sService;
+        private readonly IGuestService _guestService;
 
-        public UpdateBookingsModel(IBookingService service)
+        public UpdateBookingsModel(IBookingService service, IStandService SService, IGuestService guestService )
         {
             _service = service;
+            _sService = SService;
+            _guestService = guestService;
         }
 
         public void OnGet(int id)
         {
             Booking = _service.GetBookings().FirstOrDefault(b => b.BookingId == id);
-
+            Stands = _sService.GetStands().ToList();
+            Guests = _guestService.GetGuests().ToList();
             if (Booking == null)
             {
                 RedirectToPage("/Bookings/GetBookings");
@@ -33,7 +47,7 @@ namespace EventsPlanner.Pages.Bookings
             {
                 return Page();
             }
-
+            Booking.StandNo = StandNo;
             _service.UpdateBooking(Booking);
 
             return RedirectToPage("/Bookings/GetBookings");
